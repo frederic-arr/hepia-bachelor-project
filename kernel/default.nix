f65b9649f2d4f69b7a71215878869710aa9a7a8e
Frédéric Arroyo <frederic.arroyo@hes-so.ch>
2026-06-11 22:18:38 +0200

    fix: refactor kernel nixfile

--- kernel/default.nix
+++ kernel/default.nix
@@ -31,8 +31,6 @@ let
     inherit lib stdenv version src;
     modDirVersion = version;
     configfile    = mergedConfig;
-    allowImportFromDerivation = true;
-    kernelPatches = map (p: { patch = p; }) storePatches; # I hope I never have to patch the kernel
   };
 
   menuconfig = pkgs.writeShellApplication {
@@ -56,12 +54,11 @@ let
       export HOSTCC="${pkgs.stdenv.cc}/bin/cc"
       export HOSTCFLAGS="-I${pkgs.ncurses.dev}/include"
       export HOSTLDFLAGS="-L${pkgs.ncurses.out}/lib"
-      export NIX_LDFLAGS="-L${pkgs.ncurses.out}/lib"
 
       workdir="$PWD/kernel"
-      outfull="$PWD/kernel/config.full"
-      outmerged="$PWD/kernel/config.merged"
-      outdiff="$PWD/kernel/config.diff"
+      outfull="$workdir/config.full"
+      outmerged="$workdir/config.merged"
+      outdiff="$workdir/config.diff"
       cd "$workdir"
 
       if [ ! -d linux-${version} ]; then
@@ -77,16 +74,21 @@ let
       cp .config .config.baseline
 
       ${lib.optionalString (fragments != [ ]) ''
+        KCONFIG_CONFIG=.config.custom scripts/kconfig/merge_config.sh -m ${builtins.toString fragments}
+      ''}
+
+      ${lib.optionalString (fragments != [ ]) ''
         scripts/kconfig/merge_config.sh -m .config ${builtins.toString fragments}
       ''}
 
-      cp .config .config.current
-
+      make olddefconfig
+      cp .config .config.before
       make menuconfig
+      cp .config .config.after
 
-      scripts/diffconfig -m .config.current .config > "$outdiff"
-      scripts/diffconfig -m .config.baseline .config > "$outmerged"
-      cp .config "$outfull"
+      scripts/diffconfig -m .config.before .config.after > "$outdiff"
+      scripts/diffconfig -m .config.baseline .config.after > "$outmerged"
+      cp .config.after "$outfull"
 
       echo "Menuconfig changes: $outdiff"
       echo "Diff from ${base}: $outmerged"
