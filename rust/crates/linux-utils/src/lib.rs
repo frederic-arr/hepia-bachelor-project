bc132b531e7faa5abd284eaeb620335c90786097
Frédéric Arroyo <frederic.arroyo@hes-so.ch>
2026-06-11 22:23:48 +0200

    style: fix lint issues

--- rust/crates/linux-utils/src/lib.rs
+++ rust/crates/linux-utils/src/lib.rs
@@ -29,26 +29,27 @@ pub enum SpecialFs {
 }
 
 impl SpecialFs {
-    pub fn as_str(&self) -> &str {
+    #[must_use]
+    pub const fn as_str(&self) -> &str {
         match self {
-            SpecialFs::Sys => "sysfs",
-            SpecialFs::Proc => "proc",
-            SpecialFs::Dev => "devtmpfs",
-            SpecialFs::Cgroup2 => "cgroup2",
-            SpecialFs::Tmp => "tmpfs",
-            SpecialFs::DevPts => "devpts",
-            SpecialFs::Hugetlbfs => "hugetlbfs",
-            SpecialFs::Bpf => "bpf",
-            SpecialFs::Trace => "tracefs",
-            SpecialFs::Config => "configfs",
-            SpecialFs::Debug => "debufs",
-            SpecialFs::Security => "securityfs",
+            Self::Sys => "sysfs",
+            Self::Proc => "proc",
+            Self::Dev => "devtmpfs",
+            Self::Cgroup2 => "cgroup2",
+            Self::Tmp => "tmpfs",
+            Self::DevPts => "devpts",
+            Self::Hugetlbfs => "hugetlbfs",
+            Self::Bpf => "bpf",
+            Self::Trace => "tracefs",
+            Self::Config => "configfs",
+            Self::Debug => "debufs",
+            Self::Security => "securityfs",
         }
     }
 }
 
 pub fn mount_special<Target>(
-    fs_type: SpecialFs,
+    fs_type: &SpecialFs,
     target: Target,
     flags: MountFlags,
     options: &[&str],
