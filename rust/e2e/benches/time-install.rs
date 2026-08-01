db1257c4d2b6a58d77042f2de8c0cbc83bed275a
Frédéric Arroyo <frederic.arroyo@hes-so.ch>
2026-08-01 14:48:42 +0200

    e2e

--- rust/e2e/benches/time-install.rs
+++ rust/e2e/benches/time-install.rs
@@ -8,6 +8,7 @@ use e2e::{CosVm, Vm, random_port, wait_for_request};
 #[derive(Debug, Clone, Copy)]
 #[expect(clippy::struct_field_names, reason = "")]
 struct Measurement {
+    time_to_config: Duration,
     time_to_install: Duration,
     time_to_kernel: Duration,
     time_to_init: Duration,
@@ -17,6 +18,13 @@ struct Measurement {
     time_to_downloading_image: Duration,
     time_to_download_image: Duration,
     time_to_run_container: Duration,
+
+    time_to_kernel_post: Duration,
+    time_to_init_post: Duration,
+    time_to_supervisor_post: Duration,
+    time_to_reconcile_post: Duration,
+    time_to_dhcp_post: Duration,
+    time_to_run_container_post: Duration,
 }
 
 async fn cos_install() -> Measurement {
@@ -30,6 +38,7 @@ async fn cos_install() -> Measurement {
 
     let console_socket = vm.vm.console_socket.clone();
     let start = vm.vm.start;
+    let time_to_config = start.elapsed();
     let (a, time_to_install) = tokio::join!(vm.push_str(&data), async {
         Vm::wait_for_str(
             &console_socket,
@@ -74,9 +83,22 @@ async fn cos_install() -> Measurement {
     wait_for_request(port).await.unwrap();
     let time_to_run_container = vm.elapsed();
 
+    tokio::time::sleep(Duration::from_secs(5)).await;
+    let (
+        time_to_kernel_post,
+        time_to_init_post,
+        time_to_supervisor_post,
+        time_to_reconcile_post,
+        time_to_dhcp_post,
+    ) = vm.reboot().await.unwrap();
+
+    wait_for_request(port).await.unwrap();
+    let time_to_run_container_post = vm.elapsed();
+
     vm.kill().await.unwrap();
 
     Measurement {
+        time_to_config,
         time_to_install,
         time_to_kernel,
         time_to_init,
@@ -86,17 +108,27 @@ async fn cos_install() -> Measurement {
         time_to_downloading_image,
         time_to_download_image,
         time_to_run_container,
+
+        time_to_kernel_post,
+        time_to_init_post,
+        time_to_supervisor_post,
+        time_to_reconcile_post,
+        time_to_dhcp_post,
+        time_to_run_container_post,
     }
 }
 
 #[tokio::main(flavor = "local")]
 async fn main() {
-    const NUM_ITER: usize = 20;
+    const NUM_ITER: usize = 50;
 
     let mut cos = Vec::with_capacity(NUM_ITER);
     for i in 0..NUM_ITER {
         let data = cos_install().await;
-        println!("#{i}: {}s", data.time_to_run_container.as_secs());
+        println!(
+            "#{i}: {}s",
+            data.time_to_run_container_post.as_secs()
+        );
         cos.push(data);
     }
 
@@ -105,7 +137,8 @@ async fn main() {
         cos.into_iter()
             .map(|m| {
                 format!(
-                    "{},{},{},{},{},{},{},{},{}",
+                    "{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}",
+                    m.time_to_config.as_millis(),
                     m.time_to_install.as_millis(),
                     m.time_to_kernel.as_millis(),
                     m.time_to_init.as_millis(),
@@ -115,6 +148,12 @@ async fn main() {
                     m.time_to_downloading_image.as_millis(),
                     m.time_to_download_image.as_millis(),
                     m.time_to_run_container.as_millis(),
+                    m.time_to_kernel_post.as_millis(),
+                    m.time_to_init_post.as_millis(),
+                    m.time_to_supervisor_post.as_millis(),
+                    m.time_to_reconcile_post.as_millis(),
+                    m.time_to_dhcp_post.as_millis(),
+                    m.time_to_run_container_post.as_millis(),
                 )
             })
             .collect::<Vec<_>>()
